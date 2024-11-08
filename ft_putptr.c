89ffac3e8107de53f5397b53e124fec326ca7653
YouSsefElhIV <161429766+YouSsefElhIV@users.noreply.github.com>
2024-11-08 14:58:11 +0100

    Add files via upload

---
+++ ft_putptr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putptr.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-haya <yel-haya@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 22:14:34 by yel-haya          #+#    #+#             */
+/*   Updated: 2024/11/08 14:53:31 by yel-haya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_putptr(uintptr_t n)
+{
+	int		count;
+	char	*sym;
+
+	sym = "0123456789abcdef";
+	if (n < 0)
+	{
+		put_char('-');
+		return (ft_putptr(-n) + 1);
+	}
+	else if (n < 16)
+		return (put_char(sym[n]));
+	else
+	{
+		count = ft_putptr(n / 16);
+		return (count + ft_putptr((n % 16)));
+	}
+}
+
+int	put_ptr(uintptr_t ptr)
+{
+	int	count;
+
+	count = 0;
+	count += put_str("0x");
+	if (ptr == 0)
+		count += put_char('0');
+	else
+		count += ft_putptr(ptr);
+	return (count);
+}
